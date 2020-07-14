e63fdf380d59426c875f400f884eb764cdad335f
millbret <55172750+millbret@users.noreply.github.com>
2020-07-14 18:55:06 -0500

    Add files via upload

--- Database.java
+++ Database.java
@@ -5,56 +5,114 @@ public class Database {
     /******************************************
      * Insert all the ships as object ship
      ******************************************/
-    private Ship example;
-    private Ship example2;
-    private String[] shipName = {"example", "hi"};
+
+    /** Holds the Paris ship*/
+    private Ship Paris;
+
+    /** Holds the Punic ship*/
+    private Ship Punic;
+
+    /** Holds the Marathon ship*/
+    private Ship Marathon;
+
+    /** Holds the CCS ship*/
+    private Ship CCS;
+
+    /** Holds the CAS ship*/
+    private Ship CAS;
+
+    /** Holds the list of ship names*/
+    private String[] shipName;
+
+    /** Holds the list of ships*/
     private Ship[] ship;
 
-    private String search;
+//    private String search;
 
+    /*****************************************************************
+     * Constructor  creates a database including ships and
+     * a two lists one containing the names of the ships and another
+     * that contains the ships themselves
+     * ***************************************************************/
     public Database() {
         /*************************************************
          * Insert all of the values of the ships in here
          ************************************************/
-        this.example = new Ship();
-        this.example2 = new Ship();
-        ship = new Ship[]{example, example2};
-        this.search = "";
+        this.Paris = new Ship("Paris","Frigate","Light","UNSC",5,355,
+                40, 5,10,5,1800,3, 0,0,
+                4, 22,700,0,2);
+        this.Punic = new Ship("Punic","Carrier","VeryHeavy","UNSC",75,5500,
+                1000,1150,110,50,
+                16000,24,0,0,
+                14, 269,500,24,5);
+        this.Marathon = new Ship("Marathon","Cruiser","Medium","UNSC",25,950,
+                250,90,45,50,6000,10, 0,0,
+                7, 132,600,3,4);
+        this.CCS = new Ship("CCS","Cruiser","Medium","Coevenant",21,1650,
+                220,90,35,21,4400,5, 12000,600, 3,
+                130,700,16,3);
+        this.CAS = new Ship("CAS","Carrier","VeryHeavy","Covenant",100,10000,
+                2600,1850,120,50,14000,15,50000,2500,
+                8, 480,600,62,5);
+        ship = new Ship[]{Paris, Punic, Marathon, CCS, CAS};
+        shipName = new String[]{"Paris", "Punic", "Marathon", "CCS", "CAS"};
+//        this.search = "";
     }
 
+    /*****************************************************************
+     * Gets the info about a selected ship
+     * @param x the ship in the list we want the info from
+     * @return The information about the ship
+     * ***************************************************************/
     public String getInfo(int x){
        return (ship[x].print());
     }
 
-    public Ship getExample() {
-        return example;
-    }
+   // public Ship getExample() {
+    //    return example;
+   // }
 
-    public void setExample(Ship example) {
-        this.example = example;
-    }
+   // public void setExample(Ship example) {
+  //      this.example = example;
+  //  }
 
+    /*****************************************************************
+     * Gets the List of ship names
+     * @return list of the ships' names
+     * ***************************************************************/
     public String[] getShipName() {
         return shipName;
     }
 
+    /*****************************************************************
+     * sets the list of ship names
+     * @param ships the list of ships
+     * ***************************************************************/
     public void setShipName(String[] ships) {
         this.shipName = ships;
     }
 
+    /*****************************************************************
+     * Gets the list of ships names
+     * @return list of ships names
+     * ***************************************************************/
     public Ship[] getShip() {
         return ship;
     }
 
+    /*****************************************************************
+     * sets the ship's in the list
+     * @param ship the ships going in the list
+     * ***************************************************************/
     public void setShip(Ship[] ship) {
         this.ship = ship;
     }
 
-    public String getSearch() {
-        return search;
-    }
-
-    public void setSearch(String search) {
-        this.search = search;
-    }
+//    public String getSearch() {
+//        return search;
+//    }
+//
+//    public void setSearch(String search) {
+//        this.search = search;
+//    }
 }
--- Ship.java
+++ Ship.java
@@ -2,45 +2,66 @@ package DatabaseStuff;
 
 public class Ship {
 
+    /** Holds the name of the ship*/
     private String name;
 
+    /** Holds the name of the type of ship*/
     private String shipType;
 
+    /** Holds what the armor type of the ship is*/
     private String armorType;
 
+    /** Holds what faction the ship belongs to*/
     private String faction;
 
+    /** Holds the fleet supply of the ship*/
     private int fleetSupply;
 
+    /** Holds the number of credits the ship costs*/
     private int credits;
 
+    /** Holds the number of metals the ship costs*/
     private int metals;
 
+    /** Holds the number of crystals the ship costs*/
     private int crystals;
 
+    /** Holds the time it takes to build the ship*/
     private int buildTime;
 
+    /** Holds how much XP the ship is worth*/
     private int XP;
 
+    /** Holds the hull value of the ship*/
     private int hull;
 
+    /** Holds how fast the hull regenerates over time*/
     private int hullRestore;
 
+    /** Holds how much shielding the ship has*/
     private int shields;
 
+    /** Holds how fast the shield restores over time*/
     private int shieldRestore;
 
+    /** Holds how much armor the ship has*/
     private int armor;
 
+    /** Holds the damage per second the ship has*/
     private double DPS;
 
+    /** Holds the max speed of the ship*/
     private int maxSpeed;
 
+    /** Holds the max antimatter supply the ship has*/
     private int antimatterSupply;
 
+    /** Holds the number of weapons the ship has*/
     private int numWeapons;
 
-
+    /*****************************************************************
+     * Constructor  creates a ship with default empty values
+     * ***************************************************************/
     public Ship(){
         this.name = "";
         this.shipType = "";
@@ -63,7 +84,9 @@ public class Ship {
         this.numWeapons = 0;
     }
 
-
+    /*****************************************************************
+     * Constructor  creates a ship with the specified values given
+     * ***************************************************************/
     public Ship(String name, String type, String armor, String faction, int supply,
                 int credits, int metals, int crystals, int buildtime, int xp, int hull, int hullrestore, int shields,
                 int shieldrestore, int armorNum, double dps, int maxSpd, int antimatter, int weaponNum){
@@ -88,6 +111,10 @@ public class Ship {
         this.numWeapons = weaponNum;
     }
 
+    /*****************************************************************
+     * Gets a string with all of the values of the ship to display
+     * @return Ship's information for the gui
+     * ***************************************************************/
     public String print(){
         String info;
         return (info = new String("" +this.name +  "\nShip Type: " +this.shipType +"\nArmor Type: " +this.armorType
@@ -97,154 +124,306 @@ public class Ship {
         +this.DPS +" Max Speed: " +this.maxSpeed +"\nAntimatter: " +this.antimatterSupply +" Number of Weapons: " +this.numWeapons));
     }
 
+    /*****************************************************************
+     * Gets the Ship's name
+     * @return Ship's name
+     * ***************************************************************/
     public String getName() {
         return name;
     }
 
+    /*****************************************************************
+     * sets the ship's name
+     * @param name the name that is being set for the ship
+     * ***************************************************************/
     public void setName(String name) {
         this.name = name;
     }
 
+    /*****************************************************************
+     * Gets the Ship's type
+     * @return Ship's type
+     * ***************************************************************/
     public String getShipType() {
         return shipType;
     }
 
+    /*****************************************************************
+     * sets the ship's type
+     * @param shipType the type of ship that is being set for the ship
+     * ***************************************************************/
     public void setShipType(String shipType) {
         this.shipType = shipType;
     }
 
+    /*****************************************************************
+     * Gets the Ship's armor type
+     * @return Ship's armor type
+     * ***************************************************************/
     public String getArmorType() {
         return armorType;
     }
 
+    /*****************************************************************
+     * sets the ship's armor type
+     * @param armorType the armor type that is being set for the ship
+     * ***************************************************************/
     public void setArmorType(String armorType) {
         this.armorType = armorType;
     }
 
+    /*****************************************************************
+     * Gets the Ship's fleet supply
+     * @return Ship's fleet supply
+     * ***************************************************************/
     public int getFleetSupply() {
         return fleetSupply;
     }
 
+    /*****************************************************************
+     * sets the ship's fleet supply
+     * @param fleetSupply the fleet supply that is being set for the ship
+     * ***************************************************************/
     public void setFleetSupply(int fleetSupply) {
         this.fleetSupply = fleetSupply;
     }
 
+    /*****************************************************************
+     * Gets the Ship's credit cost
+     * @return Ship's credit cost
+     * ***************************************************************/
     public int getCredits() {
         return credits;
     }
 
+    /*****************************************************************
+     * sets the ship's credit cost
+     * @param credits the credit cost that is being set for the ship
+     * ***************************************************************/
     public void setCredits(int credits) {
         this.credits = credits;
     }
 
+    /*****************************************************************
+     * Gets the Ship's metal cost
+     * @return Ship's metal cost
+     * ***************************************************************/
     public int getMetals() {
         return metals;
     }
 
+    /*****************************************************************
+     * sets the ship's metal cost
+     * @param metals the metal cost that is being set for the ship
+     * ***************************************************************/
     public void setMetals(int metals) {
         this.metals = metals;
     }
 
+    /*****************************************************************
+     * Gets the Ship's crystal cost
+     * @return Ship's crystal cost
+     * ***************************************************************/
     public int getCrystals() {
         return crystals;
     }
 
+    /*****************************************************************
+     * sets the ship's crystal cost
+     * @param crystals the crystal cost that is being set for the ship
+     * ***************************************************************/
     public void setCrystals(int crystals) {
         this.crystals = crystals;
     }
 
+    /*****************************************************************
+     * Gets the Ship's build time
+     * @return Ship's build time
+     * ***************************************************************/
     public int getBuildTime() {
         return buildTime;
     }
 
+    /*****************************************************************
+     * sets the ship's build time
+     * @param buildTime the build time that is being set for the ship
+     * ***************************************************************/
     public void setBuildTime(int buildTime) {
         this.buildTime = buildTime;
     }
 
+    /*****************************************************************
+     * Gets the Ship's XP
+     * @return Ship's XP
+     * ***************************************************************/
     public int getXP() {
         return XP;
     }
 
+    /*****************************************************************
+     * sets the ship's XP
+     * @param XP the XP that is being set for the ship
+     * ***************************************************************/
     public void setXP(int XP) {
         this.XP = XP;
     }
 
+    /*****************************************************************
+     * Gets the Ship's hull value
+     * @return Ship's hull value
+     * ***************************************************************/
     public int getHull() {
         return hull;
     }
 
+    /*****************************************************************
+     * sets the ship's hull value
+     * @param hull the hull value that is being set for the ship
+     * ***************************************************************/
     public void setHull(int hull) {
         this.hull = hull;
     }
 
+    /*****************************************************************
+     * Gets the Ship's hull restore
+     * @return Ship's hull restore
+     * ***************************************************************/
     public int getHullRestore() {
         return hullRestore;
     }
 
+    /*****************************************************************
+     * sets the ship's hull restore
+     * @param hullRestore the hull restore that is being set for the ship
+     * ***************************************************************/
     public void setHullRestore(int hullRestore) {
         this.hullRestore = hullRestore;
     }
 
+    /*****************************************************************
+     * Gets the Ship's shield value
+     * @return Ship's shield value
+     * ***************************************************************/
     public int getShields() {
         return shields;
     }
 
+    /*****************************************************************
+     * sets the ship's shield value
+     * @param shields the shield value that is being set for the ship
+     * ***************************************************************/
     public void setShields(int shields) {
         this.shields = shields;
     }
 
+    /*****************************************************************
+     * Gets the Ship's shield restore
+     * @return Ship's shield restore
+     * ***************************************************************/
     public int getShieldRestore() {
         return shieldRestore;
     }
 
+    /*****************************************************************
+     * sets the ship's shield restore
+     * @param shieldRestore the shield restore that is being set for the ship
+     * ***************************************************************/
     public void setShieldRestore(int shieldRestore) {
         this.shieldRestore = shieldRestore;
     }
 
+    /*****************************************************************
+     * Gets the Ship's DPS
+     * @return Ship's DPS
+     * ***************************************************************/
     public double getDPS() {
         return DPS;
     }
 
+    /*****************************************************************
+     * sets the ship's DPS
+     * @param DPS the DPS that is being set for the ship
+     * ***************************************************************/
     public void setDPS(double DPS) {
         this.DPS = DPS;
     }
 
+    /*****************************************************************
+     * Gets the Ship's max speed
+     * @return Ship's max speed
+     * ***************************************************************/
     public int getMaxSpeed() {
         return maxSpeed;
     }
 
+    /*****************************************************************
+     * sets the ship's max speed
+     * @param maxSpeed the max speed that is being set for the ship
+     * ***************************************************************/
     public void setMaxSpeed(int maxSpeed) {
         this.maxSpeed = maxSpeed;
     }
 
+    /*****************************************************************
+     * Gets the Ship's armor value
+     * @return Ship's armor value
+     * ***************************************************************/
     public int getArmor() {
         return armor;
     }
 
+    /*****************************************************************
+     * sets the ship's armor value
+     * @param armor the armor value that is being set for the ship
+     * ***************************************************************/
     public void setArmor(int armor) {
         this.armor = armor;
     }
 
+    /*****************************************************************
+     * Gets the Ship's antimatter supply
+     * @return Ship's antimatter supply
+     * ***************************************************************/
     public int getAntimatterSupply() {
         return antimatterSupply;
     }
 
+    /*****************************************************************
+     * sets the ship's antimatter supply
+     * @param antimatterSupply the antimatter supply that is being set for the ship
+     * ***************************************************************/
     public void setAntimatterSupply(int antimatterSupply) {
         this.antimatterSupply = antimatterSupply;
     }
 
+    /*****************************************************************
+     * Gets the Ship's faction
+     * @return Ship's faction
+     * ***************************************************************/
     public String getFaction() {
         return faction;
     }
 
+    /*****************************************************************
+     * sets the ship's faction
+     * @param faction the faction that is being set for the ship
+     * ***************************************************************/
     public void setFaction(String faction) {
         this.faction = faction;
     }
 
+    /*****************************************************************
+     * Gets the Ship's number of weapons
+     * @return Ship's number of weapons
+     * ***************************************************************/
     public int getNumWeapons() {
         return numWeapons;
     }
 
+    /*****************************************************************
+     * sets the ship's number of weapons
+     * @param numWeapons the number of weapons that is being set for the ship
+     * ***************************************************************/
     public void setNumWeapons(int numWeapons) {
         this.numWeapons = numWeapons;
     }
